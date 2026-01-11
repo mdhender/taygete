a4e3e79871002f126c0aa6445783db860c0f9c06
Michael Henderson <mdhender@mdhender.com>
2026-01-10 20:37:53 -0500

    added example of orders

--- version.go
+++ version.go
@@ -7,8 +7,8 @@ import "github.com/maloquacious/semver"
 var (
 	version = semver.Version{
 		Major:      0,
-		Minor:      18,
-		Patch:      2,
+		Minor:      19,
+		Patch:      0,
 		PreRelease: "alpha",
 		Build:      semver.Commit(),
 	}
